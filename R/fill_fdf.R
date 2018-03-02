e9ff01c1f0a5917ae41eda35eddd5997a2374efc
OganM <ogan.mancarcii@gmail.com>
2018-03-01 20:25:08 -0800

    getFields to fill forms

---
+++ R/fill_fdf.R
@@ -0,0 +1,54 @@
+strExtract = function(x,regex){
+  match = gregexpr(regex,x, perl = TRUE)
+  if(match[[1]][1]!=-1){
+    return(regmatches(x,gregexpr(regex,x, perl = TRUE))[[1]])
+  } else{
+    return(character(0))
+  }
+}
+
+getFields <- function(input_filepath = NULL){
+  if(is.null(input_filepath)){
+    #Choose the pdf file interactively
+    input_filepath <- file.choose(new = FALSE)
+  }
+
+  fieldsTemp <- tempfile()
+  fieldsTemp <- 'tempFDF'
+
+  # generate the data field dump in a temporary file
+  system_command <- paste('pdftk',
+                          shQuote(input_filepath),
+                          'dump_data_fields','output',
+                          shQuote(fieldsTemp))
+  system(system_command)
+
+  fields <- paste0(readLines(fieldsTemp),
+                   collapse = '\n')
+  fields <- strsplit(fields, '---')[[1]][-1]
+
+  fields <- lapply(fields,function(x){
+    type <- strExtract(x,'(?<=FieldType: ).*?(?=\n)')
+    name <- strExtract(x,'(?<=FieldName: ).*?(?=\n)')
+    value <- strExtract(x,'(?<=FieldValue: ).*?(?=\n)')
+    stateOptions <- strExtract(x,'(?<=FieldStateOption: ).*?(?=\n)')
+
+    if(length(stateOptions)>0){
+      value <- factor(value,levels = stateOptions)
+    }
+
+    return(list(type = type,
+                name = name,
+                value = value))
+  })
+
+  names(fields) = sapply(fields,function(x){x$name})
+
+  return(fields)
+}
+
+
+
+# setFields = function(rdf){
+#
+# }
