2efad566068617c512c43b811674a27df59ea7b4
Ogan Mancarci <ogan.mancarci@gmail.com>
2019-06-17 19:35:46 -0700

    ugly fix for #40

--- tests/testthat/test_fill_pdf_internals.R
+++ tests/testthat/test_fill_pdf_internals.R
@@ -11,12 +11,29 @@ test_that('fdf manipulation',{
   tempPDF = tempfile(fileext = '.pdf')
   fdfLines = get_fdf_lines(pdfFileRich, tempFDF)
 
+
   fill_from_fdf(input_filepath = pdfFile,fdf_filepath =  tempFDF,output_filepath = tempPDF,overwrite = TRUE)
 
   # pdftools interact wierdly with emoji, it also inconsistently sees the spaces between different
   # # characters.
   pdfTextNewRich = pdftools::pdf_text(tempPDF)
   expect_true(grepl('½¾ ‘’”“•', pdfTextNewRich))
+
+
+  pdfFile =  system.file('testForm.pdf',package = 'staplr')
+  fdfLines = get_fdf_lines(pdfFile,tempFDF)
+  annotatedFDF = fdfAnnotate(fdfLines)
+
+  reverseFDFLines = readLines('reverseFDF')
+  reverseAnnotatedFDF = fdfAnnotate(reverseFDFLines)
+
+  reverseExclusive = unique(reverseAnnotatedFDF$fields)[!unique(reverseAnnotatedFDF$fields) %in% unique(annotatedFDF$fields)]
+
+  normalExclusive = unique(annotatedFDF$fields)[!unique(annotatedFDF$fields) %in% unique(reverseAnnotatedFDF$fields)]
+
+  testthat::expect_length(reverseExclusive,0)
+  testthat::expect_length(normalExclusive,0)
+
 
 
 })
