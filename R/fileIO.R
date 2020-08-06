0d24011ed67f2628e236746aa1607ad36598109a
OganM <ogan.mancarci@gmail.com>
2020-08-06 15:10:03 -0700

    java version checks

--- R/fileIO.R
+++ R/fileIO.R
@@ -64,6 +64,14 @@ pdftk_cmd <- function(){
   }
 
   rJava::.jinit()
+
+  jv <- rJava::.jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
+  if(substr(jv, 1L, 2L) == "1.") {
+    jvn <- as.numeric(paste0(strsplit(jv, "[.]")[[1L]][1:2], collapse = "."))
+    if(jvn < 1.8) stop("Java >= 8 is needed for staplr but it is not available")
+  }
+
+
   jOptions = getOption('staplr_java_options')
   if(is.null(jOptions)){
     jOptions = ''
