c9f8a60df1c4c2565acc9d6b0a60b1779c1c5762
OganM <ogan.mancarcii@gmail.com>
2020-08-19 13:42:35 -0700

    check for java onload and specify java 8 on description as well

---
+++ R/onload.R
@@ -0,0 +1,8 @@
+.onLoad <- function(libname, pkgname){
+  rJava::.jinit()
+  jv <- rJava::.jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
+  if(substr(jv, 1L, 2L) == "1.") {
+    jvn <- as.numeric(paste0(strsplit(jv, "[.]")[[1L]][1:2], collapse = "."))
+    if(jvn < 1.8) stop("Java >= 8 is needed for staplr but it is not available")
+  }
+}
