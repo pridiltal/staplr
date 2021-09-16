93c711cf23f2ef8bbb9148ac2fa21a5cddab3ba1
prdm0 <pedro.rafael.marinho@gmail.com>
2021-09-16 16:05:29 -0300

    Made the suggestions of https://github.com/pridiltal/staplr/pull/53#issuecomment-920510892.

--- R/staple_pages.R
+++ R/staple_pages.R
@@ -4,39 +4,39 @@
 #' @importFrom pdftools pdf_subset pdf_combine pdf_length
 #' @importFrom purrr map2_dbl walk2 flatten_dbl map_if
 #' @importFrom fs file_temp file_temp_push path_ext_remove path_file
-#' @param vec_input Vector with paths of PDF files to be combined.
-#' @param output PDF file path result of the combination.
+#' @param input_files Vector with paths of PDF files to be combined.
+#' @param output_filepath PDF file path result of the combination.
 #' @param start_pages Vector with the initial pages of each file. If \code{NA},
 #' the default, will be considered the first page.
 #' @param end_pages Vector with the final pages of each file. If \code{NA}, the
 #' default, will be considered the last page.
 #' @return
-#' In the path informed in \code{output}, the PDF file resulting from the combination
-#' of multiple files passed to \code{vec_output} will be saved.
+#' In the path informed in \code{output_filepath}, the PDF file resulting from the combination
+#' of multiple files passed to \code{input_files} will be saved.
 #' @export
 #' @examples
 #'
 #' \dontrun{
-#' combine_pdf(
-#'    vec_input =
+#' staple_pages(
+#'    input_files =
 #'       c(
 #'         "file_1.pdf",
 #'         "file_2.pdf",
 #'       ),
-#'    output = "output.pdf",
+#'    output_filepath = "output.pdf",
 #'    start_pages = c(NA, NA),
 #'    end_pages = c(NA, NA)
 #'  )
 #' }
 
-combine_pdf <- function(vec_input, output = "output.pdf", start_pages = NA, end_pages = NA) {
+staple_pages <- function(input_files, output_filepath = "output.pdf", start_pages = NA, end_pages = NA) {
 
-  if(length(start_pages) != length(vec_input) || length(end_pages) != length(vec_input))
-    stop("Start_pages and end_pages must be a vector of the same length as vec_input!")
+  if(length(start_pages) != length(input_files) || length(end_pages) != length(input_files))
+    stop("Start_pages and end_pages must be a vector of the same length as input_files!")
 
   start_pages <-
-    flatten_dbl(
-      map_if(
+    purrr::flatten_dbl(
+      purrr::map_if(
         .x = start_pages,
         .f = function(x) 1,
         .p = is.na
@@ -46,37 +46,37 @@ combine_pdf <- function(vec_input, output = "output.pdf", start_pages = NA, end_
   f <- function(x, y)
     ifelse(
       is.na(x),
-      pdf_length(vec_input[y]),
+      pdftools::pdf_length(input_files[y]),
       x
     )
 
   end_pages <-
     purrr::map2_dbl(
       .x = end_pages,
-      .y = seq_along(vec_input),
+      .y = seq_along(input_files),
       .f = f
     )
 
   files <-
-    file_temp_push(
+    fs::file_temp_push(
       glue(
-        "{tempdir()}/{path_ext_remove(path_file(output))}_{1L:length(vec_input)}.pdf"
+        "{tempdir()}/{fs::path_ext_remove(fs::path_file(output_filepath))}_{1L:length(input_files)}.pdf"
       )
     )
 
   one_step <- function(x, y) {
-    pdf_subset(
+    pdftools::pdf_subset(
         input = x,
-        output = file_temp(),
+        output = fs::file_temp(),
         pages = start_pages[y]:end_pages[y]
       )
   }
 
-  walk2(
-    .x = vec_input,
-    .y = 1L:length(vec_input),
+  purrr::walk2(
+    .x = input_files,
+    .y = 1L:length(input_files),
     .f = ~ one_step(.x, .y)
   )
 
-  pdf_combine(input = files, output = output)
+  pdftools::pdf_combine(input = files, output = output_filepath)
 }
