88ae4108d5596b6ad5872b436816d575bbd793e7
William May <wmay@hey.com>
2021-11-07 01:58:01 -0400

    Rename jar file to pdftk_jar for clarity

--- R/onload.R
+++ R/onload.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname){
-  staplr_jar <- system.file('pdftk-java/pdftk.jar', package = pkgname, mustWork = TRUE)
-  rJava::.jpackage(pkgname, morePaths = staplr_jar, lib.loc = libname)
+  pdftk_jar <- system.file('pdftk-java/pdftk.jar', package = pkgname, mustWork = TRUE)
+  rJava::.jpackage(pkgname, morePaths = pdftk_jar, lib.loc = libname)
   jv <- rJava::.jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
   if(substr(jv, 1L, 2L) == "1.") {
     jvn <- as.numeric(paste0(strsplit(jv, "[.]")[[1L]][1:2], collapse = "."))
